541a8ecef19989a5faae58b065310f3f8053c9cb
Trent McGinnis <46269691+TrentMcGinnis@users.noreply.github.com>
2020-08-14 02:10:23 -0400

    start random stuff

--- index.js
+++ index.js
@@ -11,8 +11,22 @@ app.get("/", (req, res, next) => {
 });
 
 app.get("/agents", (req, res, next) => {
-    
-})
+    const agent_data = require("./public/config/agents.json");
+    const random_agent = agent_data.agents[Math.round(Math.random() * (agent_data.agents.length - 1))];
+    res.render('agents', {agents: agent_data});
+});
+
+app.get("/maps", (req, res, next) => {
+    const map_data = require("./public/config/maps.json");
+    const random_map = map_data.maps[Math.round(Math.random() * (map_data.maps.length - 1))];
+    res.render('random', {image: random_map.image, name: random_map.id.toUpperCase()});
+});
+
+app.get("/weapons", (req, res, next) => {
+    const weapon_data = require("./public/config/weapons.json");
+    const random_weapon = weapon_data.weapons[Math.round(Math.random() * (weapon_data.weapons.length - 1))];
+    res.render('random', {image: random_weapon.image, name: random_weapon.id.toUpperCase()});
+});
 
 app.listen(process.env.PORT, () => {
     console.log("SERVER ON LISTENING ON PORT " + process.env.PORT);
