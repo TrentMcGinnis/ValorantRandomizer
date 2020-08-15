52411c9ddf076fa6e9db733c7191fe4e56ebff26
Trent McGinnis <46269691+TrentMcGinnis@users.noreply.github.com>
2020-08-15 14:33:45 -0400

    randomizer done

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 const express = require("express");
 const app = express();
-const fetch = require("node-fetch");
 require("dotenv").config();
 
 app.set('view engine', 'pug');
 app.use('/public', express.static('public'));
 
 app.get("/", (req, res, next) => {
-    res.render("index");
+    res.redirect("/randomgame");
 });
 
-app.get("/agents", (req, res, next) => {
+app.get("/random_agent", (req, res, next) => {
     const agent_data = require("./public/config/agents.json");
     const random_agent = agent_data.agents[Math.round(Math.random() * (agent_data.agents.length - 1))];
-    res.render('agents', {agents: agent_data});
+    res.send(random_agent);
 });
 
-app.get("/maps", (req, res, next) => {
+app.get("/random_map", (req, res, next) => {
     const map_data = require("./public/config/maps.json");
     const random_map = map_data.maps[Math.round(Math.random() * (map_data.maps.length - 1))];
-    res.render('random', {image: random_map.image, name: random_map.id.toUpperCase()});
+    const random_side = random_map.sides[Math.round(Math.random() * (random_map.sides.length - 1))];
+    const random_map_data = {image: random_map.image, id: random_map.id, side: random_side}
+    res.send(random_map_data);
 });
 
-app.get("/weapons", (req, res, next) => {
+app.get("/random_weapon", (req, res, next) => {
+    const budget = req.query.budget;
     const weapon_data = require("./public/config/weapons.json");
-    const random_weapon = weapon_data.weapons[Math.round(Math.random() * (weapon_data.weapons.length - 1))];
-    res.render('random', {image: random_weapon.image, name: random_weapon.id.toUpperCase()});
+    const budgeted_weapons = weapon_data.weapons.filter(weapon => weapon.cost <= budget);
+    const random_weapon = budgeted_weapons[Math.round(Math.random() * (budgeted_weapons.length - 1))];
+    res.send(random_weapon);
+});
+
+app.get("/randomgame", (req, res, next) => {
+    const agent_data = require("./public/config/agents.json");
+    const map_data = require("./public/config/maps.json");
+    const weapon_data = require("./public/config/weapons.json");
+
+    const first_round_weapons = weapon_data.weapons.filter(weapon => weapon.cost <= 800)
+
+    const random_map = map_data.maps[Math.round(Math.random() * (map_data.maps.length - 1))];
+    const random_side = random_map.sides[Math.round(Math.random() * (random_map.sides.length - 1))];
+    const random_map_data = {image: random_map.image, id: random_map.id, side: random_side}
+    const random_agent = agent_data.agents[Math.round(Math.random() * (agent_data.agents.length - 1))];
+    const random_weapon = first_round_weapons[Math.round(Math.random() * (first_round_weapons.length - 1))];
+    res.render('random', {map: random_map_data, agent: random_agent, weapon: random_weapon});
 });
 
 app.listen(process.env.PORT, () => {
---
+++ views/random.js
@@ -0,0 +1,28 @@
+async function reroll(id) {
+    let data = null;
+    let respone = null;
+    switch (id) {
+        case "map":
+            response = await fetch("/random_map");
+            data = await response.json();
+            document.getElementById('map_image').src = data.image;
+            document.getElementById('map_id').innerText = data.id;
+            document.getElementById('map_side').innerText = "Start the game on side " + data.side;
+            break;
+        case "agent":
+            response = await fetch("/random_agent");
+            data = await response.json();
+            document.getElementById('agent_image').src = data.image;
+            document.getElementById('agent_id').innerText = data.id;
+            break;
+        case "weapon":
+        default:
+            const budget_number = document.getElementById('budget').value;
+            response = await fetch("/random_weapon" + "?budget=" + budget_number);
+            data = await response.json();
+            document.getElementById('weapon_image').src = data.image;
+            document.getElementById('weapon_id').innerText = data.id;
+            break;
+    }
+
+}
